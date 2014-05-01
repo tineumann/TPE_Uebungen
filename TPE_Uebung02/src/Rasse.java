2258abd7f578fe3254c1c780e963c42780b711b8
Connez <c.linus.schneider@web.de>
2014-05-01 18:19:17 +0200

    enum Rasse, interface Wesen

--- TPE_Uebung02/src/Rasse.java
+++ TPE_Uebung02/src/Rasse.java
@@ -1,4 +1,59 @@
 
-public enum Rasse {
- ORK, MENSCH, UNTOTER, NACHTELF
+public enum Rasse implements Wesen {
+ ORK(100, 33, 1, 30.0, 4.0, 150),
+ UNTOTER(120, 16, 2, 30.0, 1.6, 70),
+ MENSCH(140, 40, 2, 40.0, 10.0, 110),
+ NACHTELF(120, 15, 3, 20.0, 2.9, 145);
+ 
+ private int lebenspunkte;
+ private int schaden;
+ private int geschwindigkeit;
+ private int kosten;
+ private double ruestung;
+ private double spezialattribut;
+
+ 
+ private Rasse(int lebenspunkte, int schaden, int geschwindigkeit,
+		 double ruestung, double spezialattribut, int kosten){
+	 
+	 this.lebenspunkte = lebenspunkte;
+	 this.schaden = schaden;
+	 this.geschwindigkeit = geschwindigkeit;
+	 this.ruestung = ruestung;
+	 this.spezialattribut = spezialattribut;
+	 this.kosten = kosten;
+ }
+ 
+ public int getLebenspunkte(){
+	 return this.lebenspunkte;
+ }
+ 
+ public int getSchaden(){
+	 return this.schaden;
+ }
+ 
+ public int getGeschwindigkeit(){
+	 return this.geschwindigkeit;
+ }
+ 
+ public double getRuestung(){
+	 return this.ruestung;
+ }
+ 
+ public double getSpezialattribut(){
+	 return this.spezialattribut;
+ }
+ 
+ public int getKosten(){
+	 return this.kosten;
+ }
+ 
+ @Override 
+ public boolean isLebendig(){
+	 if(this.lebenspunkte > 0)
+		 return true;
+	 else
+		 return false;
+	 }
+ 
 }
--- TPE_Uebung02/src/Wesen.java
+++ TPE_Uebung02/src/Wesen.java
@@ -1,28 +1,9 @@
-/**
- * Die abstrakte Klasse Wesen stellt ledeiglich Attribute und Methoden ihrer
- * Subklassen dar. Es sollen keine Objekte vom Typ Wesen erzeugt werden, da
- * es nur Objekte ihrer Subklassen gibt.
- * 
- * @author Constantin Schneider
- * @author Timo Neumann
- *
- */
-public class Wesen {
+interface Wesen {
 
-	private int lebenspunkte;
-	private int ruestung;
-	private int schaden;
-	private int geschwindigkeit;
-	private double spezialattribut;
+	public boolean isLebendig();
 	
-	public boolean isLebendig(){
-		if(this.lebenspunkte > 0)
-			return true;
-		else return false;
-	}
-	
-	public void attack(Squad s){
+	/*public void attack(Squad s){
 		
 	}
-	
+	*/
 }
