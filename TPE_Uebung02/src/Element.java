bf8c4dd3a123603bbc2f644ab8352aa72ee3e881
Connez <c.linus.schneider@web.de>
2014-05-04 16:29:05 +0200

    Held und Element angepasst

--- TPE_Uebung02/src/Element.java
+++ TPE_Uebung02/src/Element.java
@@ -1,3 +1,30 @@
 public enum Element {
-	ERDE, WASSER, FEUER, LUFT
+	ERDE, WASSER, FEUER, LUFT;
+
+	public boolean isUeberlegen(Element gegnerElement) {
+		switch (this) {
+		case FEUER:
+			if (gegnerElement == Element.LUFT)
+				return true;
+			else
+				return false;
+		case ERDE:
+			if (gegnerElement == Element.WASSER)
+				return true;
+			else
+				return false;
+		case WASSER:
+			if (gegnerElement == Element.FEUER)
+				return true;
+			else
+				return false;
+		case LUFT:
+			if (gegnerElement == Element.ERDE)
+				return true;
+			else
+				return false;
+		default:
+			return false;
+		}
+	}
 }
--- TPE_Uebung02/src/Held.java
+++ TPE_Uebung02/src/Held.java
@@ -1,10 +1,30 @@
 public abstract class Held extends Wesen {
 	Element element;
 	double bonusfaktor;
-	
-	public Held(Rasse rasse, Element element, double bonusfaktor){
+
+	public Held(Rasse rasse, Element element, double bonusfaktor) {
 		super(rasse);
 		this.element = element;
 		this.bonusfaktor = bonusfaktor;
+	}
+
+	@Override
+	public void attack(Kaempfer r) {
+		double damage;
+		Wesen gegner;
+
+		if (r instanceof Wesen) {
+			gegner = ((Wesen) r);
+			damage = getDamage(gegner) * this.bonusfaktor;
+			
+			if (r instanceof Held) {
+				if (this.element.isUeberlegen(((Held)gegner).element)) {
+					damage *= 2;
+				}
+			}
+			
+			damage = damage / (gegner.getRasse().getRuestung() / 100);
+			gegner.setLebenspunkte(gegner.getLebenspunkte() - damage);		
+		}
 	}
 }
