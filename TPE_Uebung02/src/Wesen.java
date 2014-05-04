0ef1cac714a3e6775300f2535bd800d97ffe7b89
Timo Neumann <ti.neumann@me.com>
2014-05-04 17:59:39 +0200

    toString in Wesen

--- TPE_Uebung02/src/Wesen.java
+++ TPE_Uebung02/src/Wesen.java
@@ -1,78 +1,83 @@
 /**
- * Die Klasse WesenImpl stellt lediglich Attribute und Methoden ihrer
- * Subklassen dar. Es sollen keine Objekte vom Typ Wesen erzeugt werden, da
- * es nur Objekte ihrer Subklassen gibt.
+ * Die Klasse WesenImpl stellt lediglich Attribute und Methoden ihrer Subklassen
+ * dar. Es sollen keine Objekte vom Typ Wesen erzeugt werden, da es nur Objekte
+ * ihrer Subklassen gibt.
  * 
  * @author Constantin Schneider
  * @author Timo Neumann
- *
+ * 
  */
 public abstract class Wesen implements Kaempfer {
 
 	private double lebenspunkte;
 	private Rasse rasse;
-	
-	public Wesen(Rasse rasse){
+
+	public Wesen(Rasse rasse) {
 		this.rasse = rasse;
-		
+
 		this.lebenspunkte = this.rasse.getLebenspunkte();
 	}
-	
-	public boolean isLebendig(){
-		if(this.lebenspunkte > 0)
+
+	public boolean isLebendig() {
+		if (this.lebenspunkte > 0)
 			return true;
-		else return false;
+		else
+			return false;
 	}
-	
+
 	@Override
-	public void attack(Kaempfer r){
+	public void attack(Kaempfer r) {
 		double damage = 0;
-		if(r instanceof Wesen){
-			Wesen gegner = ((Wesen)r);
-			
-			if(r instanceof Erzmagier && Erzmagier.isAbsorption()){
+		if (r instanceof Wesen) {
+			Wesen gegner = ((Wesen) r);
+
+			if (r instanceof Erzmagier && Erzmagier.isAbsorption()) {
 				damage = 0;
-			} 
-			
-			else {
-				damage = getDamage(gegner) 
-					/ (gegner.rasse.getRuestung() / 100);
 			}
-				
-			gegner.setLebenspunkte(gegner.lebenspunkte - damage);	
-		}	
+
+			else {
+				damage = getDamage(gegner) / (gegner.rasse.getRuestung() / 100);
+			}
+
+			gegner.setLebenspunkte(gegner.lebenspunkte - damage);
+		}
 	}
-	
+
 	/**
 	 * Errechnet den Schaden, den ein Wesen bei einem Angriff erzeugt.
 	 */
-	public double getDamage(Wesen r){
-		double damage = this.getRasse().getGeschwindigkeit() 
+	public double getDamage(Wesen r) {
+		double damage = this.getRasse().getGeschwindigkeit()
 				* this.getRasse().getSchaden()
 				* this.getRasse().getSpezialattribut();
-		
-		if(r.rasse == Rasse.MENSCH){
+
+		if (r.rasse == Rasse.MENSCH) {
 			damage = beschraenkeSchaden(damage);
 		}
-		
+
 		return damage;
 	}
-	
-	public double beschraenkeSchaden(double damage){
+
+	public double beschraenkeSchaden(double damage) {
 		return damage * 0.9d;
 	}
-	
-	public Rasse getRasse(){
+
+	public Rasse getRasse() {
 		return this.rasse;
 	}
 
-	public double getLebenspunkte(){
+	public double getLebenspunkte() {
 		return this.lebenspunkte;
 	}
-	
-	public void setLebenspunkte(double lebenspunkte){
+
+	public void setLebenspunkte(double lebenspunkte) {
 		this.lebenspunkte = lebenspunkte;
 	}
-	
-}
 
+	@Override
+	public String toString() {
+		return new String(this.rasse.toString() + ", LP: "
+				+ Double.toString(this.getLebenspunkte()));
+	}
+
+}
--- TPE_Uebung02/src/WesenFactory.java
+++ TPE_Uebung02/src/WesenFactory.java
@@ -1,15 +1,3 @@
 public class WesenFactory {
 
-	public static Kaempfer[] getKaempfer(Rasse rasse, int geld) {
-		int anzahlKaempfer = geld / rasse.getKosten();
-		Kaempfer[] kaempfer = new Kaempfer[anzahlKaempfer];
-		if (anzahlKaempfer > 0) {
-			for (int i = 0; i < anzahlKaempfer; i++) {
-				
-				
-				kaempfer[i] = new Wesen(rasse);
-			}
-		}
-		return kaempfer;
-	}
 }
