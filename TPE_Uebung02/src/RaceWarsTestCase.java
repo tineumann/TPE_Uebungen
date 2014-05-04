768c567ecfa5a9ca22a7322119c4b19737a213b0
Connez <c.linus.schneider@web.de>
2014-05-04 18:51:43 +0200

    paar tests

--- TPE_Uebung02/src/RaceWarsTestCase.java
+++ TPE_Uebung02/src/RaceWarsTestCase.java
@@ -2,11 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class RaceWarsTestCase {
+	public class RaceWarsTestCase {
 
-	@Test
-	
+		@Test
+		public void testIsLebendig(){
+			Mensch m1 = new Mensch();
+			assertTrue(m1.isLebendig());
 		
-	}
+			Mensch m2 = new Mensch();
+			m2.setLebenspunkte(0);
+			assertFalse(m2.isLebendig());
+		
+			Mensch m3 = new Mensch();
+			m3.setLebenspunkte(-100);
+			assertFalse(m2.isLebendig());
+		}
+		
+		@Test
+		public void testAttack(){
+			Mensch m1 = new Mensch();
+			Mensch m2 = new Mensch();
+			m1.attack(m2);
+			assertFalse(m2.isLebendig());
+		}
+	
 
 }
