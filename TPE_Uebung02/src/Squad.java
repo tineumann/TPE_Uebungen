cb6619c2151e937cff277a011554522f254fde67
Timo Neumann <ti.neumann@me.com>
2014-05-04 14:42:12 +0200

    squad erstellt

--- TPE_Uebung02/src/Squad.java
+++ TPE_Uebung02/src/Squad.java
@@ -1,4 +1,22 @@
 
 public class Squad{
-	Kaempfer[] kaempfer = new Kaempfer[10];
+	private Kaempfer[] kaempfer;
+	public Kaempfer[] getKaempfer() {
+		return kaempfer;
+	}
+
+	public void setKaempfer(Kaempfer[] kaempfer) {
+		this.kaempfer = kaempfer;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private String name;
+	
+	public Squad(String name, Kaempfer[] kaempfer){
+		this.kaempfer = kaempfer;
+		this.name = name;
+	}
 }
