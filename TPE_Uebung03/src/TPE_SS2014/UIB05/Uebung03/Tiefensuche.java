5333916a0e56ed08a25c68d4f02d7fec10cb6152
Connez <c.linus.schneider@web.de>
2014-05-20 17:26:10 +0200

    Klassen erstellt

---
+++ TPE_Uebung03/src/TPE_SS2014/UIB05/Uebung03/Tiefensuche.java
@@ -0,0 +1,17 @@
+package TPE_SS2014.UIB05.Uebung03;
+
+public abstract class Tiefensuche implements SearchStrategy {
+
+	@Override
+	public Node search(T value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
