e8ef1a3bbb00545cf83eb9b7a89021da962bbe18
tineumann <ti.neumann@me.com>
2014-05-22 22:50:37 +0200

    searchstrategy angepasst

--- TPE_Uebung03/src/TPE_SS2014/UIB05/Uebung03/SearchStrategy.java
+++ TPE_Uebung03/src/TPE_SS2014/UIB05/Uebung03/SearchStrategy.java
@@ -2,6 +2,6 @@ package TPE_SS2014.UIB05.Uebung03;
 
 //TODO: Constantin
 public interface SearchStrategy {
-	public Node search(T value);
+	public Node<?> search(Object value);
 	public String getPath();
 }
