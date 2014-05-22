c0353b154290b06d4a344bad2c49c58f4d114ead
tineumann <ti.neumann@me.com>
2014-05-22 22:37:30 +0200

    NodeList

---
+++ TPE_Uebung03/src/TPE_SS2014/UIB05/Uebung03/NodeListImpl.java
@@ -0,0 +1,9 @@
+package TPE_SS2014.UIB05.Uebung03;
+
+import java.util.LinkedList;
+
+public class NodeListImpl extends LinkedList<Node> implements NodeList  {
+
+	private static final long serialVersionUID = 1L;
+
+}
